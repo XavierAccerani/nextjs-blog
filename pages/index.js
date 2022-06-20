075286f138898ac734c359bc97036d8d8d6a7371
Xavier Accerani <xavier@MacBook-Air-de-Xavier.local>
2022-06-20 18:51:11 +0200

    changes home page text

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi my name is Xavier and I'm a frontend developper in Nextjs</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website that I built with{" "}
+          <a href="https://nextjs.org/learn">a Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
